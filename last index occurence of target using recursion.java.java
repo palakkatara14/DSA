5ec2767a2171640ad54e782fb9a0382e5bf72729
palak katara <122154471+palakkatara14@users.noreply.github.com>
2024-02-10 14:26:21 +0530

    Add files via upload

---
+++ last index occurence of target using recursion.java.java
@@ -0,0 +1,21 @@
+
+public class Main{
+    public static void main(String[] args){
+        int[] arr={1,2,3,2,2};
+        int target=3;
+        System.out.println(find(arr,target,arr.length-1));
+    }
+    public static int find(int[] arr,int target,int index){
+            if(index==0){
+                return -1;
+            }
+            
+            
+            
+            
+            if(arr[index]==target){
+                return index;
+            }
+            return find(arr,target,index-1);
+    }
+}
